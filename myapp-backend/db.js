d0fe7b94bbe573721534d4625303c36acd4adb1b
Jeremy Cardona <jeremy.cardona@upr.edu>
2018-06-11 16:13:30 -0400

    db,js

--- myapp-backend/db.js
+++ myapp-backend/db.js
@@ -5,5 +5,4 @@ var documentSchema = new mongoose.Schema({
 });
 mongoose.model('Document', documentSchema);  
 //mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds157621.mlab.com:57621/markdown-editor'); 
-mongoose.connect('mongodb://<"jeremy.cardona">:<"Cardona1%40">@ds157621.mlab.com:57621/markdown-editor'); 
   
